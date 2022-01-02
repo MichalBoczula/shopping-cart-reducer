3a0a72510b562aca3148c694c17f6c33e471b7ba
Michal Boczula <michal.boczula0101@gmail.com>
2022-01-02 18:11:05 +0100

    added basic functionality

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,39 @@
 const reducer = (state, action) => {
+  switch (action.type) {
+    case "CLEAR_CART":
+      return { ...state, cart: [] };
+    case "REMOVE":
+      return {
+        ...state,
+        cart: state.cart.filter((x) => x.id !== action.payload),
+      };
+    case "INCREASE":
+      let tempState = state.cart.map((x) => {
+        if (x.id === action.payload) {
+          console.log(x);
+          return { ...x, amount: x.amount + 1 };
+        }
+        return x;
+      });
+      return {
+        ...state,
+        cart: tempState,
+      };
+    case "DECREASE":
+      let tempState2 = state.cart
+        .map((x) => {
+          if (x.id === action.payload) {
+            return { ...x, amount: x.amount - 1 };
+          }
+          return x;
+        })
+        .filter((item) => item.amount !== 0);
+      return {
+        ...state,
+        cart: tempState2,
+      };
+  }
+
   return state;
 };
 
